8dabf87b9efc321d534f5d2523bdcc4837e3845b
Braden Wockner <bwockner@euclideon.com>
2020-08-21 14:52:00 +1000

    fixed .java for android

--- Assets/Plugins/EuclideonUdSDK/udSDK/UdSDK.java
+++ Assets/Plugins/EuclideonUdSDK/udSDK/UdSDK.java
@@ -3,9 +3,9 @@ package com.euclideon;
 import android.content.Context;
 
 /**
-    Euclideon Vault SDK library initialization
+    Euclideon udSDK library initialization
 */
-public class VaultSDK {
+public class udSDK {
     // This function should be called first and sets up the native code
     // so it can call into the Java classes
     public static native int nativeSetupJNI(Context context);
